574e92114c401854be9c9f3f7a467e7668214980
zhling <1309455849@qq.com>
2024-05-27 14:53:25 +0800

    显示用户名和头像

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -7,8 +7,12 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
-          <i class="el-icon-caret-bottom" />
+          <!-- 显示头像 -->
+          <img :src="avatar" class="user-avatar">
+          <!-- 显示用户名称 -->
+          <span class="name">{{ name }}</span>
+          <!-- 图标 -->
+          <i class="el-icon-setting" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
           <router-link to="/">
@@ -42,9 +46,11 @@ export default {
     Hamburger
   },
   computed: {
+    // 引入头像和用户名称
     ...mapGetters([
       'sidebar',
-      'avatar'
+      'avatar',
+      'name'
     ])
   },
   methods: {
@@ -117,12 +123,23 @@ export default {
       .avatar-wrapper {
         margin-top: 5px;
         position: relative;
+        display: flex;
+        align-items: center;
 
         .user-avatar {
           cursor: pointer;
-          width: 40px;
-          height: 40px;
-          border-radius: 10px;
+          width: 30px;
+          height: 30px;
+          border-radius: 50%;
+        }
+        .name{
+          cursor: pointer;
+          margin-right: 10px;
+          font-size: 16px;
+        }
+        .el-icon-setting{
+          cursor: pointer;
+          font-size: 20px;
         }
 
         .el-icon-caret-bottom {
--- src/store/getters.js
+++ src/store/getters.js
@@ -3,7 +3,7 @@ const getters = {
   device: state => state.app.device,
   token: state => state.user.token,
   userId: state => state.user.userInfo.userId,
-  avatar: state => state.user.avatar,
-  name: state => state.user.name
+  avatar: state => state.user.userInfo.staffPhoto, // 向外暴露头像
+  name: state => state.user.userInfo.username  //向外暴露用户名称
 }
 export default getters
