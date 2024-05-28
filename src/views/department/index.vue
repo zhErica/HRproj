fb834d4f2cad401750fa046d4101b280c956a8dc
zhling <1309455849@qq.com>
2024-05-28 15:00:52 +0800

    树形组件的应用

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="container">
         <div class="app-container">
-            组织架构
+            <!-- 展示树形结构 -->
+            <el-tree default-expand-all :data="depts" :props="defaulProps"/>
         </div>
     </div>
   
@@ -9,10 +10,25 @@
 
 <script>
 export default {
-  name: 'Department'
+  name: 'Department',
+  data(){
+    return {
+        depts:[{name:'传智教育',children:[{name:'总裁办'},{name:'行政部'},{name:"人事部"}]
+    }],//数据属性
+        defaulProps:{
+            label:'name', //要显示的字段名称
+            children:'children', // 读取子节点的字段名
+
+        }
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+.app-container{
+    padding:30px 140px;
+    font-size: 14px;
+}
 
 </style>
