ff3bf321d1271a4c4e68284e3c551823e36c0798
zhling <1309455849@qq.com>
2024-05-28 16:21:02 +0800

    获取组织架构的数据

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,34 +1,74 @@
 <template>
-    <div class="container">
-        <div class="app-container">
-            <!-- 展示树形结构 -->
-            <el-tree default-expand-all :data="depts" :props="defaulProps"/>
-        </div>
+  <div class="container">
+    <div class="app-container">
+      <!-- 展示树形结构 -->
+      <el-tree default-expand-all :data="depts" :props="defaulProps">
+        <!-- 节点结构 -->
+        <!-- 作用域插槽 v-slot="{node, data}" 只能作用在template -->
+        <template v-slot="{data}">
+          <el-row
+            style="width: 100%; height: 40px"
+            type="flex"
+            justify="space-between"
+            align="middle"
+          >
+            <el-col>{{data.name }}</el-col>
+            <el-col :span="4">
+              <span class="tree-manager">{{data.managerName}}</span>
+              <el-dropdown>
+                <!-- 显示区域内容 -->
+                <span class="el-dropdown-link">
+                  操作<i class="el-icon-arrow-down el-icon--right"></i>
+                </span>
+                <!-- 下拉菜单选项 -->
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>添加子部门</el-dropdown-item>
+                  <el-dropdown-item>编辑部门</el-dropdown-item>
+                  <el-dropdown-item>删除</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
+        </template>
+      </el-tree>
     </div>
-  
+  </div>
 </template>
 
 <script>
+import {getDepartment} from '@/api/department'
 export default {
-  name: 'Department',
-  data(){
+  name: "Department",
+  data() {
     return {
-        depts:[{name:'传智教育',children:[{name:'总裁办'},{name:'行政部'},{name:"人事部"}]
-    }],//数据属性
-        defaulProps:{
-            label:'name', //要显示的字段名称
-            children:'children', // 读取子节点的字段名
-
-        }
+      depts: [], //数据属性
+      defaulProps: {
+        label: "name", //要显示的字段名称
+        children: "children", // 读取子节点的字段名
+      },
+    };
+  },
+  created(){
+    this.getDepartment() //调用获取数据接口
+  },
+  methods:{
+    // 封装好一个方法
+    async getDepartment(){
+        const result = await getDepartment()
+        this.depts = result
     }
   }
-}
+};
 </script>
 
 <style scoped>
-.app-container{
-    padding:30px 140px;
-    font-size: 14px;
+.app-container {
+  padding: 30px 140px;
+  font-size: 14px;
 }
-
+.tree-manager {
+  width: 50px;
+  display: inline-block;
+  margin-right: 25px;
+}
 </style>
