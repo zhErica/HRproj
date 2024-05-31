feea054d058c70e9be6e0ee0d7255a79f27074f3
zhling <1309455849@qq.com>
2024-05-31 13:11:26 +0800

    角色管理页面提交

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="container">
     <div class="app-container">
-      角色管理
+      <!-- 角色管理内容 -->
+      <div class="role-operate">
+        <el-button type="primary" size="mini">添加角色</el-button>
+      </div>
+      <!-- 放置表格 -->
+      <el-table>
+        <!-- 放置列 -->
+        <el-table-column align="center" width="200px" label="角色"></el-table-column>
+        <el-table-column align="center" width="200px" label="启用"></el-table-column>
+        <el-table-column align="center" label="描述"></el-table-column>
+        <el-table-column align="center" label="操作"></el-table-column>
+      </el-table>
+      <!-- 放置分页组件 -->
+      <el-row type="flex" justify="end" style="height: 60px;" align="middle">
+      <!-- 放置分页组件 -->
+      <el-pagination layout="prev,pager,next">
+
+      </el-pagination>
+      </el-row>
+
     </div>
   </div>
 </template>
@@ -10,3 +29,8 @@ export default {
   name: 'Role'
 }
 </script>
+<style scoped>
+.role-operate{
+  padding: 10px;
+}
+</style>
