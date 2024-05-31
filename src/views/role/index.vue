8756a089da2f6cfad7960deee2148ea83d2c5774
zhling <1309455849@qq.com>
2024-05-31 13:48:25 +0800

    获取角色列表数据

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -6,27 +6,40 @@
         <el-button type="primary" size="mini">添加角色</el-button>
       </div>
       <!-- 放置表格 -->
-      <el-table>
+      <el-table :data="list">
         <!-- 放置列 -->
-        <el-table-column align="center" width="200px" label="角色"></el-table-column>
-        <el-table-column align="center" width="200px" label="启用"></el-table-column>
-        <el-table-column align="center" label="描述"></el-table-column>
+        <el-table-column prop="name" align="center" width="200px" label="角色"></el-table-column>
+        <el-table-column prop="state" align="center" width="200px" label="启用"></el-table-column>
+        <el-table-column prop="description" align="center" label="描述"></el-table-column>
         <el-table-column align="center" label="操作"></el-table-column>
       </el-table>
       <!-- 放置分页组件 -->
       <el-row type="flex" justify="end" style="height: 60px;" align="middle">
       <!-- 放置分页组件 -->
       <el-pagination layout="prev,pager,next">
-
       </el-pagination>
       </el-row>
-
     </div>
   </div>
 </template>
 <script>
+import {getRoleList} from '@/api/role'
 export default {
-  name: 'Role'
+  name: 'Role',
+  data(){
+    return {
+      list:[]
+    }
+  },
+  created(){
+    this.getRoleList()
+  },
+  methods:{
+    async getRoleList(){
+      const {rows} = await getRoleList()
+      this.list = rows   // 赋值数据
+    }
+  }
 }
 </script>
 <style scoped>
