3d4e6493ad1b037451b39e8011f6ab866440e399
zhling <1309455849@qq.com>
2024-06-03 16:23:02 +0800

    左侧树的数据加载

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -4,6 +4,8 @@
       <div class="left">
         <el-input style="margin-bottom:10px" type="text" prefix-icon="el-icon-search" size="small" placeholder="输入员工姓名全员搜索" />
         <!-- 树形组件 -->
+        <el-tree :data="depts" :props="defaultProps" default-expand-all :expand-on-click-node="false" :highlight-current="true">
+        </el-tree>
       </div>
       <div class="right">
         <el-row class="opeate-tools" type="flex" justify="end">
@@ -19,8 +21,29 @@
 </template>
 
 <script>
+import { getDepartment} from '@/api/department'
+import{transListToTreeData} from '@/utils'
 export default {
-  name: 'Employee'
+  name: 'Employee',
+  data(){
+    return {
+      depts:[], // 组织数据
+      defaultProps:{
+        label:'name',
+        children :'children'
+      }
+    }
+  },
+  created(){
+    this.getDepartment()
+  },
+  methods:{
+    async getDepartment(){
+      // 调用api接口
+      // 递归方法 将列表转化为树形
+      this.depts =transListToTreeData(await getDepartment(),0) 
+    }
+  }
 }
 </script>
 
