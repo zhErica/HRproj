29dafc1365c49ebb89f8d5fa5f77166d217aba3b
zhling <1309455849@qq.com>
2024-06-04 19:25:50 +0800

    员工导入模板下载

--- src/api/employee.js
+++ src/api/employee.js
@@ -15,6 +15,16 @@ export function exportEmployee(){
     return request({
         url:'/sys/user/export',
         method:'get',
+        responseType:'blob'  // 使用blob接收二进制文件流
+    })
+}
+
+//下载员工的导入模板
+
+export function getExportTemplate(){
+    return request({
+        url:'/sys/user/import/template',
+        method:'get',
         responseType:'blob'
     })
 }
--- src/views/employee/components/import-excel.vue
+++ src/views/employee/components/import-excel.vue
@@ -15,7 +15,7 @@
           >
           <div class="drop">
             <i class="el-icon-upload" />
-            <el-button type="text">下载导入模板</el-button>
+            <el-button type="text" @click="getTemplate">下载导入模板</el-button>
             <span>将文件拖到此处或
               <el-button type="text">点击上传</el-button>
             </span>
@@ -29,7 +29,8 @@
     </el-dialog>
   </template>
   <script>
-  
+  import {getExportTemplate} from '@/api/employee'
+  import FileSaver from 'file-saver';
   export default {
     props: {
       showExcelDialog: {
@@ -38,7 +39,10 @@
       }
     },
     methods: {
-  
+        async getTemplate(){
+            const data = await getExportTemplate()
+            FileSaver.saveAs(data,'员工导入模板.xlsx')
+        }
     }
   }
   </script>
