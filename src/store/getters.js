50c946c68bc6a981187e4e199176085ab96a8732
zhling <1309455849@qq.com>
2024-06-06 15:54:05 +0800

    首页个人信息的显示

--- src/store/getters.js
+++ src/store/getters.js
@@ -5,6 +5,8 @@ const getters = {
   userId: state => state.user.userInfo.userId,
   avatar: state => state.user.userInfo.staffPhoto, // 向外暴露头像
   name: state => state.user.userInfo.username,  //向外暴露用户名称
+  company:state =>state.user.userInfo.company, //向外暴露公司名称
+  departmentName: state =>state.user.userInfo.departmentName,
   routes:state => state.user.routes //路由
 }
 export default getters
